2be43274beed4c3265f0dee69a50ccd64225485b
Mohammed BY <mohammed.beny@hotmail.fr>
2021-06-16 15:59:05 +0100

    scrollable

--- Components/LoginForm.js
+++ Components/LoginForm.js
@@ -105,6 +105,7 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
+        marginTop:"10%"
     },
     textcontainer:{
         fontWeight: "bold",
