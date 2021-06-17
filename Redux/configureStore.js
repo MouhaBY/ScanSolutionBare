98d16a28dcf9a3818dc767ed98a94c6f9783c006
Mohammed BY <mohammed.beny@hotmail.fr>
2021-06-17 12:38:48 +0100

    'redux'

---
+++ Redux/configureStore.js
@@ -0,0 +1,11 @@
+import { combineReducers, createStore } from 'redux';
+import authReducer from './Reducers/authenticationReducer'
+
+
+const reducer = combineReducers({
+    authReducer
+})
+
+const store = createStore(reducer)
+
+export default store
